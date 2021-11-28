9c4aa8941144d44dc0c7444a437c3ade1844aaa9
Madhurshani <madhurshanisivan@gmail.com>
2021-11-28 07:32:25 +0530

    add to main

--- models/person.js
+++ models/person.js
@@ -4,8 +4,6 @@ const neo4j = require("neo4j-driver");
 require("dotenv").config();
 const { url, username, password, database } = process.env;
 console.log(url, username, password, database);
-const driver = neo4j.driver(url, neo4j.auth.basic(username, password), {
-  /* encrypted: 'ENCRYPTION_OFF' */
-});
+const driver = neo4j.driver(url, neo4j.auth.basic(username, password), {});
 
 const session = driver.session({ database });
