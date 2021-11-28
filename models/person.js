421b4ca934d6a17ec9fd89d76fe481b14cbfabee
Madhurshani <madhurshanisivan@gmail.com>
2021-11-28 07:19:15 +0530

    configure neo4j

---
+++ models/person.js
@@ -0,0 +1,11 @@
+// npm install --save neo4j-driver
+// node example.js
+const neo4j = require("neo4j-driver");
+require("dotenv").config();
+const { url, username, password, database } = process.env;
+console.log(url, username, password, database);
+const driver = neo4j.driver(url, neo4j.auth.basic(username, password), {
+  /* encrypted: 'ENCRYPTION_OFF' */
+});
+
+const session = driver.session({ database });
